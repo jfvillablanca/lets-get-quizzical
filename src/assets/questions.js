b26112e15c392d966ea99b2cbb064a9a6f0ce8a2
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-07 15:05:18 +0800

    Add temporary data

---
+++ src/assets/questions.js
@@ -0,0 +1,56 @@
+const results = [
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "hard",
+        question:
+            "Which of these was the name of a bug found in April 2014 in the publicly available OpenSSL cryptography library?",
+        correct_answer: "Heartbleed",
+        incorrect_answers: ["Shellshock", "Corrupted Blood", "Shellscript"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "hard",
+        question:
+            "Who is the original author of the realtime physics engine called PhysX?",
+        correct_answer: "NovodeX",
+        incorrect_answers: ["Ageia", "Nvidia", "AMD"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "hard",
+        question:
+            "Which of the following is the oldest of these computers by release date?",
+        correct_answer: "TRS-80",
+        incorrect_answers: ["Commodore 64", "ZX Spectrum", "Apple 3"],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "hard",
+        question: "The acronym &quot;RIP&quot; stands for which of these?",
+        correct_answer: "Routing Information Protocol",
+        incorrect_answers: [
+            "Runtime Instance Processes",
+            "Regular Interval Processes",
+            "Routine Inspection Protocol",
+        ],
+    },
+    {
+        category: "Science: Computers",
+        type: "multiple",
+        difficulty: "hard",
+        question:
+            "What type of sound chip does the Super Nintendo Entertainment System (SNES) have?",
+        correct_answer: "ADPCM Sampler",
+        incorrect_answers: [
+            "FM Synthesizer",
+            "Programmable Sound Generator (PSG)",
+            "PCM Sampler",
+        ],
+    },
+];
+
+export default results;
