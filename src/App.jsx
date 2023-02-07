b7e41d20bc671eb8facf723ec4b6f0d34b23e3b9
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-07 09:36:39 +0800

    Import Intro.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Toggle from "react-toggle";
 import "./assets/react-toggle.css";
+import Intro from "./components/Intro.jsx";
 
 function App() {
     return <div className='App'></div>;
