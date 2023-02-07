18a7e96f26e314366f76b94e277124069af224d8
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-07 09:38:18 +0800

    Add feat: themes for .App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import "./App.scss";
 import Toggle from "react-toggle";
 import "./assets/react-toggle.css";
+import { Icon } from "@iconify/react";
 import Intro from "./components/Intro.jsx";
 
 function App() {
-    return <div className='App'></div>;
+    const [theme, setTheme] = useState("dark");
+
+    return (
+        <div className={`App ${theme}`}>
+            <Toggle
+                className='theme-toggle'
+                icons={{
+                    checked: (
+                        <Icon icon='material-symbols:light-mode-outline' />
+                    ),
+                    unchecked: (
+                        <Icon icon='material-symbols:dark-mode-outline' />
+                    ),
+                }}
+            />
+        </div>
+    );
 }
 
 export default App;
