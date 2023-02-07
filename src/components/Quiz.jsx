5f5a461ea43efe7d0c7f695ab4b4cb70f281925b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-07 15:12:14 +0800

    Add Quiz and Question components

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,13 @@
+export default function Quiz() {
+    return (
+        <div className='quiz'>
+            <Question />
+        </div>
+    );
+}
+function Question({ question, choices }) {
+    return (
+        <section className='quiz-question'>
+        </section>
+    );
+}
