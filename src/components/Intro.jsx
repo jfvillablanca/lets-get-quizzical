a1937e335494a2135f43b9f7b51e4612eed1486d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-07 13:28:10 +0800

    Add high score placeholder

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,8 @@
 export default function Intro() {
     return (
         <div className='intro'>
-            <h1>👾 Quiz Me Daddy 👾</h1>
-            <h3></h3>
+            <h1>💭😩 Quiz Me Daddy 🥴💦</h1>
+            <h3>High Score: 0</h3>
             <button>Start the quiz</button>
         </div>
     );
