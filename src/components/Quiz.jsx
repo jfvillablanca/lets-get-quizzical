1a12a460faeb96c3f7113876070d575067f4bb0f
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-07 15:16:17 +0800

    Add feat: get prop values

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -8,6 +8,14 @@ export default function Quiz() {
 function Question({ question, choices }) {
     return (
         <section className='quiz-question'>
+            <h1>{question}</h1>
+            <div className='quiz-choices'>
+                <button className='quiz-choice-1'>{choices[0].choice}</button>
+                <button className='quiz-choice-1'>{choices[1].choice}</button>
+                <button className='quiz-choice-1'>{choices[2].choice}</button>
+                <button className='quiz-choice-1'>{choices[3].choice}</button>
+            </div>
+            <hr />
         </section>
     );
 }
