3821fabecad014d72e613bf4ec376e049c804e4a
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-10 08:44:15 +0800

    Chore: cleanup of unused lines and assets

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,6 @@ export default function Intro({ handleIntroClick }) {
     return (
         <div className='intro'>
             <h1>💭😩 Quiz Me Daddy 🥴💦</h1>
-            <h3>High Score: 0</h3>
             <button onClick={handleIntroClick}>Start the quiz</button>
         </div>
     );
