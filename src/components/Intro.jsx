019fa598d7321e3cdec68596984e47f043b3e33b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-28 18:17:56 +0800

    Feat: add no. of questions category

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,60 @@
-export default function Intro({ handleIntroClick }) {
+export default function Intro({
+    handleIntroClick,
+    handleCategoryChange,
+    selectedCategory,
+}) {
     return (
         <div className='intro'>
             <h1>💭😩 Quiz Me Daddy 🥴💦</h1>
-            <button onClick={handleIntroClick}>Start the quiz</button>
+            <div className='category'>
+                <label
+                    className={
+                        "category-questions" +
+                        (selectedCategory === "5 questions" ? " active" : "")
+                    }
+                >
+                    <input
+                        type='radio'
+                        name='question-qty'
+                        value='5 questions'
+                        onChange={handleCategoryChange}
+                    />
+                    5 questions
+                </label>
+                <label
+                    className={
+                        "category-questions" +
+                        (selectedCategory === "10 questions" ? " active" : "")
+                    }
+                >
+                    <input
+                        type='radio'
+                        name='question-qty'
+                        value='10 questions'
+                        onChange={handleCategoryChange}
+                    />
+                    10 questions
+                </label>
+                <label
+                    className={
+                        "category-questions" +
+                        (selectedCategory === "15 questions" ? " active" : "")
+                    }
+                >
+                    <input
+                        type='radio'
+                        name='question-qty'
+                        value='15 questions'
+                        onChange={handleCategoryChange}
+                    />
+                    15 questions
+                </label>
+            </div>
+            <button onClick={handleIntroClick}>
+                {!selectedCategory
+                    ? "Select no. of questions"
+                    : "Start the quiz!"}
+            </button>
         </div>
     );
 }
